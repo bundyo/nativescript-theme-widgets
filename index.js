a5d649955ff2f2bad7a01af3f1aa18ba94648de3
Kamen Bundev <bundyo@gmail.com>
2019-06-19 18:49:54 +0300

    Remove some comments

--- index.js
+++ index.js
@@ -5,13 +5,3 @@ export * from "./NTMenu/NTMenu";
 export * from "./NTInput/NTInput";
 export * from "./NTModalPage/NTModalPage";
 export * from "./NTActionBar/NTActionBar";
-
-// function addPlatformClass() {
-//     return function (cls) {
-//         cls.prototype.className = isAndroid ? "-android" : "-ios";
-//     };
-// }
-//
-// decorate([
-//     addPlatformClass()
-// ], Frame);
